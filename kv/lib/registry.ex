6b31970fdad0984b757f0f791b5bf390dfd1da5e
Geoff deRosenroll <geoffderosenroll@gmail.com>
2019-07-12 00:59:02 -0700

    finishing GenServer section of elixir start-up guide
    Learn to monitor processes and use the messages we can have them generate when they die

--- kv/lib/registry.ex
+++ kv/lib/registry.ex
@@ -1,36 +1,94 @@
 defmodule KV.Registry do
+  @moduledoc """
+  Handles spawning of new KV.Bucket processes and assigns unique name strings
+  to each.
+  """
   use GenServer
 
   # Callbacks
-
   @impl true
   def init(:ok) do
     # initialize the state
-    {:ok, %{}}
+    names = %{}
+    refs = %{}
+    {:ok, {names, refs}}
   end
 
   @impl true
-  def handle_call({:lookup, name}, _from, names) do
+  def handle_call({:lookup, name}, _from, state) do
+    # can pattern match out the desired Map from the state here
+    {names, _} = state
     # don't care where it came from here, GenServer handles the reply
-    {:reply, Map.fetch(names, name), names}
+    {:reply, Map.fetch(names, name), state}
   end
 
   @impl true
-  def handle_call({:list_names}, _from, names) do
-    {:reply, (for pair <- names, do: elem(pair, 0)), names}
+  def handle_call({:list_names}, _from, state) do
+    {names, _} = state
+    {:reply, for(pair <- names, do: elem(pair, 0)), state}
   end
 
+  @doc """
+  Note: can also pattern match out the parts of the state in the args to the
+  function as well. As opposed to pattern used above in the handle_call[s]
+  """
   @impl true
-  def handle_cast({:create, name}, names) do
-    if Map.has_key?(names, name) do
-      # return same state, no new bucket created/registered
-      {:noreply, names}
-    else
-      # create a new bucket process
-      {:ok, bucket} = KV.Bucket.start_link([])
-      # add a new name => bucket pair to the registry state map (return)
-      {:noreply, Map.put(names, name, bucket)}
+  def handle_cast({:create, name}, {names, refs}) do
+    # if Map.has_key?(names, name) do
+    #  # return same state, no new bucket created/registered
+    #  {:noreply, {names, refs}}
+    # else
+    #  # create a new bucket process and get monitor reference
+    #  {:ok, bucket} = KV.Bucket.start_link([])
+    #  ref = Process.monitor(bucket)
+    #  # add a new name => bucket pair to the registry state map (return)
+    #  names = Map.put(names, name, bucket)
+    #  refs = Map.put(refs, ref, name)
+    #  {:noreply, {refs, names}}
+    # end
+
+    # the same thing, but with case pattern matching. I believe this is
+    # similar to what the if macro is tranformed in to. Really just wanted to
+    # practice the syntax.
+    case Map.has_key?(names, name) do
+      true ->
+        {:noreply, {names, refs}}
+
+      false ->
+        {:ok, bucket} = KV.Bucket.start_link([])
+        ref = Process.monitor(bucket)
+
+        # update state Maps
+        names = Map.put(names, name, bucket)
+        refs = Map.put(refs, ref, name)
+
+        # send updated state back to server
+        {:noreply, {names, refs}}
     end
+  end
+
+  @doc """
+  This will be called when it recieves a message indicating that a bucket has
+  stopped (a tuple matching this pattern).
+  """
+  @impl true
+  def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
+    # get the name corresponding to the process reference (, and the refs map
+    # with that ref removed)
+    {name, refs} = Map.pop(refs, ref)
+    names = Map.delete(names, name)
+    {:noreply, {names, refs}}
+  end
+
+  @doc """
+  A catch-all handle_info/2 callback that does nothing with the messages we
+  don't care about that our server might recieve. All messages that are not
+  GenServer calls and casts must come in through handle_info, including
+  regular send/2 messages.
+  """
+  @impl true
+  def handle_info(_msg, state) do
+    {:noreply, state}
   end
 
   ## Client API
--- kv/test/kv/registry_test.exs
+++ kv/test/kv/registry_test.exs
@@ -2,23 +2,45 @@ defmodule KV.RegistryTest do
   use ExUnit.Case
 
   setup do
-    {:ok, registry} = KV.Registry.start_link([])
+    # rather than using Registry's start_link/1, use the start_supervised!/2
+    # function (optional initial state value is second arg, default: [])
+    # injected by ExUnit.Case. This ensures that the service is stopped before
+    # running the next test (great in case they access the same things).
+    registry = start_supervised!(KV.Registry)
     %{registry: registry}
   end
 
   test "registers processes", %{registry: registry} do
     # create a bucket process named "cart"
     KV.Registry.create(registry, "cart")
+
     # attempting to create existing process does nothing (no error)
     KV.Registry.create(registry, "cart")
+
     # new bucket process
     KV.Registry.create(registry, "wishlist")
 
     # lookup returns {:ok, pid} for existing bucket
     assert {:ok, bucket} = KV.Registry.lookup(registry, "cart")
+
+    # the bucket works?
+    KV.Bucket.put(bucket, "dark souls", 1)
+    assert KV.Bucket.get(bucket, "dark souls") == 1
+
     # non-existent name returns :error on lookup
     assert KV.Registry.lookup(registry, "shopping") == :error
+
     # returns list of all names in the registry
     assert KV.Registry.list_names(registry) == ["cart", "wishlist"]
+  end
+
+  test "removes buckets on exit", %{registry: registry} do
+    # create bucket and stop it
+    KV.Registry.create(registry, "cart")
+    {:ok, bucket} = KV.Registry.lookup(registry, "cart")
+    Agent.stop(bucket)
+
+    # check that "cart" no longer exists
+    assert KV.Registry.lookup(registry, "cart") == :error
   end
 end
