44233fea7bdfe6762d7d206d7c2b6dbf075ee2fd
Geoff deRosenroll <geoffderosenroll@gmail.com>
2019-07-19 01:33:32 -0700

    overhaul registry to use ETS for global/concurrently readable name table
    Followed guide roughly, learning about ETS, first use of Erlang module. Compiled but not yet tested.

--- kv/lib/registry.ex
+++ kv/lib/registry.ex
@@ -1,60 +1,74 @@
 defmodule KV.Registry do
   @moduledoc """
   Handles spawning of new KV.Bucket processes and assigns unique name strings
-  to each.
+  to each. This updated version uses the :ets module from Erlang so that this
+  server will not become a bottle nec in the case that many processes want to
+  access buckets (thus would need to make calls to the registry to get pids).
   """
   use GenServer
 
-  # Callbacks
+  ## Client API
+
+  @doc """
+  Starts the registry.
+  __MODULE__ means the current module.
+  Returns {:ok, registry_pid} on success.
+  """
+  def start_link(opts) do
+    server_name = Keyword.fetch!(opts, :name)
+    GenServer.start_link(__MODULE__, server_name, opts)
+  end
+
+  @doc """
+  Create a bucket associated with the given `name` in `server`.
+  No reply (return) is generated for the client.
+  """
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
+
+  @doc """
+  Looks up the bucket pid for `name` stored in ETS, no need to call a server.
+  Returns `[{name, pid}]` if the bucket exists, `[]` otherwise.
+  We use the pin ^ operator here to match against the value of an existing
+  variable, rather than assign the value from the right into it.
+  """
+  def lookup(server, name) do
+    case :ets.lookup(server, name) do
+      [{^name, pid}] -> {:ok, pid}
+      [] -> :error
+    end
+  end
+
+  @doc """
+  Get list of all the bucket names tracked by the registry. Use fun2ms to
+  create a query function that can be passed to the select function.
+  """
+  def list_names(table) do
+    get_names = :ets.fun2ms(fn {name, _pid} -> name end)
+    :ets.select(table, get_names)
+  end
+
+  ## Server Callbacks
   @impl true
-  def init(:ok) do
-    # initialize the state
-    names = %{}
+  def init(table_name) do
+    # initialize the globally readable ETS table
+    names = :ets.new(table_name, [:named_table, read_concurrency: true])
     refs = %{}
     {:ok, {names, refs}}
   end
 
-  @impl true
-  def handle_call({:lookup, name}, _from, state) do
-    # can pattern match out the desired Map from the state here
-    {names, _} = state
-    # don't care where it came from here, GenServer handles the reply
-    {:reply, Map.fetch(names, name), state}
-  end
-
-  @impl true
-  def handle_call({:list_names}, _from, state) do
-    {names, _} = state
-    {:reply, for(pair <- names, do: elem(pair, 0)), state}
-  end
-
   @doc """
-  Note: can also pattern match out the parts of the state in the args to the
-  function as well. As opposed to pattern used above in the handle_call[s]
+  If name is in the registry already, do nothing. Otherwise create a new
+  dynamically supervised bucket, and store it's name, pid, and reference.
   """
   @impl true
   def handle_cast({:create, name}, {names, refs}) do
-    # if Map.has_key?(names, name) do
-    #  # return same state, no new bucket created/registered
-    #  {:noreply, {names, refs}}
-    # else
-    #  # create a new bucket process and get monitor reference
-    #  {:ok, bucket} = KV.Bucket.start_link([])
-    #  ref = Process.monitor(bucket)
-    #  # add a new name => bucket pair to the registry state map (return)
-    #  names = Map.put(names, name, bucket)
-    #  refs = Map.put(refs, ref, name)
-    #  {:noreply, {refs, names}}
-    # end
-
-    # the same thing, but with case pattern matching. I believe this is
-    # similar to what the if macro is tranformed in to. Really just wanted to
-    # practice the syntax.
-    case Map.has_key?(names, name) do
-      true ->
+    case lookup(names, name) do
+      {:ok, _pid} ->
         {:noreply, {names, refs}}
 
-      false ->
+      :error ->
         {:ok, bucket} =
           DynamicSupervisor.start_child(
             KV.BucketSupervisor,
@@ -63,26 +77,25 @@ defmodule KV.Registry do
 
         # get a reference for the process (to match against :DOWN message)
         ref = Process.monitor(bucket)
-
-        # update state Maps
-        names = Map.put(names, name, bucket)
         refs = Map.put(refs, ref, name)
 
-        # send updated state back to server
+        # update ETS names table
+        :ets.insert(names, {name, bucket})
+
+        # return the updated names and refs to the server
         {:noreply, {names, refs}}
     end
   end
 
   @doc """
   This will be called when it recieves a message indicating that a bucket has
-  stopped (a tuple matching this pattern).
+  stopped (a tuple matching this pattern). Get the name corresponding to the
+  process reference (, and the refs map with that ref removed).
   """
   @impl true
   def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
-    # get the name corresponding to the process reference (, and the refs map
-    # with that ref removed)
     {name, refs} = Map.pop(refs, ref)
-    names = Map.delete(names, name)
+    :ets.delete(names, name)
     {:noreply, {names, refs}}
   end
 
@@ -95,41 +108,5 @@ defmodule KV.Registry do
   @impl true
   def handle_info(_msg, state) do
     {:noreply, state}
-  end
-
-  ## Client API
-  # Simple abstractions of the GenServer call in this case.
-  # Could be in a seperate module if desired (replace __MODULE__ with name)
-
-  @doc """
-  Starts the registry.
-  __MODULE__ means the current module.
-  Returns {:ok, registry_pid} on success.
-  """
-  def start_link(opts) do
-    GenServer.start_link(__MODULE__, :ok, opts)
-  end
-
-  @doc """
-  Looks up the bucket pid for `name` stored in `server`.
-  Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
-  """
-  def lookup(server, name) do
-    GenServer.call(server, {:lookup, name})
-  end
-
-  @doc """
-  Returns a list of the named processes in the registry
-  """
-  def list_names(server) do
-    GenServer.call(server, {:list_names})
-  end
-
-  @doc """
-  Create a bucket associated with the given `name` in `server`.
-  No reply (return) is generated for the client.
-  """
-  def create(server, name) do
-    GenServer.cast(server, {:create, name})
   end
 end
