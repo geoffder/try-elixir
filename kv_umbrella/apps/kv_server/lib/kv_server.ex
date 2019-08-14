c5eb25d25973b73c84c2638e63428dfa0a8205b8
Geoff deRosenroll <geoffderosenroll@gmail.com>
2019-08-14 01:08:09 -0700

    Basic TCP server that echos client sockets requests

--- kv_umbrella/apps/kv_server/lib/kv_server.ex
+++ kv_umbrella/apps/kv_server/lib/kv_server.ex
@@ -1,18 +1,69 @@
 defmodule KVServer do
   @moduledoc """
-  Documentation for KVServer.
+  TCP server implemenation using Erlang's :gen_tcp
   """
+  require Logger
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> KVServer.hello()
-      :world
-
+  :gen_tcp options used for this server:
+  -> `:binary` - receives data as binaries (instead of lists)
+  -> `packet: :line` - receive data line by line
+  -> `active: false` - blocks on `:gen_tcp.recv/2` until data is available
+  -> `reuseaddr: true` - allows us to reuse the address if listener crashes
   """
-  def hello do
-    :world
+  def accept(port) do
+    {:ok, socket} =
+      :gen_tcp.listen(
+        port,
+        [:binary, packet: :line, active: false, reuseaddr: true]
+      )
+
+    Logger.info("Accepting connections on port #{port}")
+    loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+
+    # Start temporary process to serve the incoming request to the socket.
+    {:ok, pid} =
+      Task.Supervisor.start_child(
+        KVServer.TaskSupervisor,
+        fn ->
+          serve(client)
+        end
+      )
+
+    # set the newly created child process to be the "controlling process" of
+    # the client socket. This way the acceptor will not bring all of the
+    # clients if it crashes. By default, sockets are tied to the process that
+    # accepts them (which in this case is the Task that ran accept(port)).
+    :ok = :gen_tcp.controlling_process(client, pid)
+
+    # wait for the next request
+    loop_acceptor(socket)
+  end
+
+  defp serve(socket) do
+    # Read line at a time from the accepted socket connection and write the
+    # lines back to the same socket.
+
+    # Pipeline equivalent to write_line(read_line(socket), socket), so first
+    # arg is the data moving through the pipe, and we provide the connected
+    # socket as the second argument (to write to).
+    socket
+    |> read_line()
+    |> write_line(socket)
+
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
   end
 end
--- kv_umbrella/apps/kv_server/lib/kv_server/application.ex
+++ kv_umbrella/apps/kv_server/lib/kv_server/application.ex
@@ -5,10 +5,27 @@ defmodule KVServer.Application do
 
   use Application
 
+  @doc """
+  Start the accept loop of the server as a Task. Specify the port to listen on
+  in command line when running application, e.g. `PORT=#### mix run --no-halt`
+  Default port is 4040 if argument not given. Here we override the default
+  child specification of Task to use a :permanent restart strategy, as
+  opposed to temporary (so it will be restarted on failure).
+
+  Additionally, we'll use Task.Supervisor to start temporary task processes
+  from our loop_acceptor with a :one_for_one strategy so that failed requests
+  and broken connections do not bring down our application. Our server which
+  is running as a Task depends on this, so we need to start it first.
+  """
   def start(_type, _args) do
+    port = String.to_integer(System.get_env("PORT") || "4040")
+
     children = [
-      # Starts a worker by calling: KVServer.Worker.start_link(arg)
-      # {KVServer.Worker, arg}
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      Supervisor.child_spec(
+        {Task, fn -> KVServer.accept(port) end},
+        restart: :permanent
+      )
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
